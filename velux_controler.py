50b641666f23123445007f9ae1caf8a11594c8c1
Sébastien Rinsoz <sebastien@rinsoz.org>
2022-05-27 08:50:10 +0200

    Update velux_controler.py

--- velux_controler.py
+++ velux_controler.py
@@ -1,12 +1,15 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 import argparse
 import json
 
 # import Yoctopuce library
+from yoctopuce.yocto_display import *
 from yoctopuce.yocto_relay import *
 from yoctopuce.yocto_carbondioxide import *
+from yoctopuce.yocto_temperature import *
+from yoctopuce.yocto_anbutton import *
 
 
 class VeluxZone(object):
@@ -16,102 +19,181 @@ class VeluxZone(object):
         self._desription = descr
         self._closeRelay = YRelay.FindRelay(hwid_close)
         self._openRelay = YRelay.FindRelay(hwid_open)
+        self._is_open = False
 
-    def open(self):
-        self._openRelay.pulse(200)
+    def open(self, force):
+        try:
+            if force or not self._is_open:
+                self._openRelay.pulse(200)
+                self._is_open = True
+            return True
+        except YAPI_Exception:
+            print("Unable to open zone %s (%s)" % (self.name, self._desription))
+            return False
 
-    def close(self):
-        self._closeRelay.pulse(200)
+    def close(self, force):
+        try:
+            if force or self._is_open:
+                self._closeRelay.pulse(200)
+                self._is_open = False
+            return True
+        except YAPI_Exception:
+            print("Unable to close zone %s (%s)" % (self.name, self._desription))
+            return False
 
-    def check_relays(self):
+    def isOpen(self):
+        return self._is_open
+
+    def check_relays(self, exit_on_error=True):
         if not self._closeRelay.isOnline():
-            sys.exit("Relay %s for zone %s is not online" % (self._desription, self._closeRelay.describe()))
+            if exit_on_error:
+                sys.exit("Relay %s for zone %s is not online" % (self._desription, self._closeRelay.describe()))
+            else:
+                return False
         if not self._openRelay.isOnline():
-            sys.exit("Relay %s for zone %s is not online" % (self._desription, self._openRelay.describe()))
+            if exit_on_error:
+                sys.exit("Relay %s for zone %s is not online" % (self._desription, self._openRelay.describe()))
+            else:
+                return False
+        return True
 
     def bind(self):
-        self._closeRelay.pulse(1300)
-        pass
+        try:
+            self._closeRelay.pulse(1300)
+            return True
+        except YAPI_Exception:
+            print("Unable to bind zone %s (%s)" % (self.name, self._desription))
+            return False
+
+
+class VeluxButton(object):
+
+    def __init__(self, controler, cmd, targets, hwid):
+        self._controler = controler
+        self._cmd = cmd.lower()
+        self._target = targets
+        self._anButton = YAnButton.FindAnButton(hwid)
+        self._anButton.isOnline()
+        self._anButton.registerValueCallback(self.AnButtonCB)
+
+    def AnButtonCB(self, anbutton, value):
+        print("AnCB:" + anbutton.get_hardwareId() + "=" + value)
+        if int(value) != 0:
+            # value != 0 meant that the button is pressed
+            if (self._cmd == 'open'):
+                self._controler.open(self._target, True)
+            elif self._cmd == 'close':
+                self._controler.close(self._target, True)
 
 
 class VeluxControler(object):
-    def __init__(self, config_file, targets, verbose):
-        self.closed = True
-        self.co2_open_limit = 900
-        self.co2_close_limit = 700
+    def __init__(self, config_file, verbose):
+        self._alive_counter = 0
         self.verbose = verbose
-        with open('config.json', "r") as f:
+        with open(config_file, "r") as f:
             config = json.load(f)
-
-        self.co2_open_limit = config['co2']['limit']
-        self.co2_close_limit = config['co2']['limit'] - 100
-
+        self.co2_open_limit = config['co2']['open_limit']
+        self.co2_close_limit = config['co2']['close_limit']
+        self.max_open_time = config['co2']['max_open_time']
+        self.manually_open = False
         if self.verbose:
             print("Use Yoctopuce library : " + YAPI.GetAPIVersion())
+        YAPI.RegisterLogFunction(self.log)
         errmsg = YRefParam()
         for hub in config['yoctohubs']:
             if YAPI.RegisterHub(hub, errmsg) != YAPI.SUCCESS:
                 sys.exit("Unable connect to %s : %s" % (hub, errmsg.value))
-
-        self.co2sensor = YCarbonDioxide.FirstCarbonDioxide()
-        if self.co2sensor is None:
-            sys.exit("No C02 sensor found. Please plug an Yocto-CO2 or Yocto-CO2-V2 on an USB port")
-
+        self.displays = []
+        for disp_id in config['display']:
+            self.displays.append(YDisplay.FindDisplay(disp_id))
+        self.temperature = YTemperature.FindTemperature(config["temperature_sensor"])
+        self.co2sensor = YCarbonDioxide.FindCarbonDioxide(config['co2']['sensor_id'])
         self.zones = []
-
-        include_all_zone = False
-        if len(targets) == 0 or targets[0] == "All":
-            include_all_zone = True
-
         for zone_name in config['zones']:
             z = config['zones'][zone_name]
-            if zone_name in targets or include_all_zone:
-                zone = VeluxZone(zone_name, z['descr'], z['close_relay'], z['open_relay'])
-                zone.check_relays()
-                self.zones.append(zone)
-
+            zone = VeluxZone(zone_name, z['descr'], z['close_relay'], z['open_relay'])
+            self.zones.append(zone)
+        self.buttons = []
+        for but in config['buttons']:
+            vbut = VeluxButton(self, but["action"], but["zones"], but['hwid'])
+            self.buttons.append(vbut)
         if self.verbose:
-            msg = "Targeted zones:"
+            msg = "Zones:"
             for r in self.zones:
                 msg += " " + r.name
             print(msg)
 
-    def open(self):
-        for z in self.zones:
-            z.open()
-            YAPI.Sleep(500)
+    def log(self, line):
+        print("YAPI:" + line)
 
-    def close(self):
+    def open(self, targets, force=False):
+        if self.verbose:
+            if len(targets) == 0:
+                print("Open all zones")
+            else:
+                print("Open zones " + ' '.join(targets))
+        if force:
+            self.manually_open = True
         for z in self.zones:
-            z.close()
-            YAPI.Sleep(500)
+            if len(targets) == 0 or z.name in targets:
+                if z.open(force):
+                    YAPI.Sleep(500)
 
-    def auto(self):
+    def close(self, targets, force=False):
+        if self.verbose:
+            if len(targets) == 0:
+                print("Close all zones")
+            else:
+                print("close zones " + ' '.join(targets))
+        if force:
+            self.manually_open = False
+        for z in self.zones:
+            if len(targets) == 0 or z.name in targets:
+                if z.close(force):
+                    YAPI.Sleep(500)
+
+    def moduleArrival(self, ymodule):
+        """
+
+        :type ymodule: YModule
+        """
+        if self.verbose:
+            print("Device plug: " + ymodule.get_serialNumber())
+
+        ids = ymodule.get_functionIds("AnButton")
+        print(ids)
+
+    def moduleRemoval(self, ymodule):
+        """
+
+        :type ymodule: YModule
+        """
+        if self.verbose:
+            print("Device unplug: " + ymodule.get_serialNumber())
+
+    def auto2(self):
+        # star with all Velux closed
+        self.close([], True)
+        self.manually_open = False
         if self.verbose:
             print("Co2 limit is set to %d ppm" % self.co2_open_limit)
-        self.close()
-        self.closed = True
-        sleep_delay = 5000
-        while self.co2sensor.isOnline():
-            value = self.co2sensor.get_currentValue()
-            if self.verbose:
-                print("C02 value: %dppm" % value)
-            if value > self.co2_open_limit:
-                # speed up C02 measure when the windows are open
-                sleep_delay = 5000  # 5 seconds
-                if self.closed:
-                    print("C02 concentrations (%dppm) is beyond the %dppm limit: open the windows" %
-                          (value, self.co2_open_limit))
-                    self.open()
-                    self.closed = False
-            elif value < self.co2_close_limit:
-                sleep_delay = 60000  # 1 minute
-                if not self.closed:
-                    self.close()
-                    self.closed = True
-            YAPI.Sleep(sleep_delay)
-        if not self.closed:
-            self.close()
+        # display clean up
+        for disp in self.displays:
+            if (disp.isOnline()):
+                disp.resetAll()
+        while True:
+            self.refreshDisplays()
+            if self.co2sensor.isOnline():
+                value = self.co2sensor.get_currentValue()
+                if value > self.co2_open_limit:
+                    if self.verbose:
+                        print("C02 concentrations (%dppm) is beyond the %dppm limit: open the windows" %
+                              (value, self.co2_open_limit))
+                    self.open([])
+                elif value < self.co2_close_limit and not self.manually_open:
+                    self.close([])
+            YAPI.UpdateDeviceList()
+            YAPI.Sleep(1000)
 
     def bind(self):
         z = self.zones[0]
@@ -119,7 +201,8 @@ class VeluxControler(object):
             print("Bind %s" % z.name)
         z.bind()
 
-    def release(self):
+    @staticmethod
+    def release():
         YAPI.FreeAPI()
 
     def read_co2(self):
@@ -133,6 +216,40 @@ class VeluxControler(object):
         self.co2sensor.set_highestValue(value)
         self.co2sensor.set_lowestValue(value)
 
+    def refreshDisplays(self):
+        for disp in self.displays:
+            if not disp.isOnline():
+                continue
+            try:
+                # retrieve the display size
+                w = disp.get_displayWidth()
+                h = disp.get_displayHeight()
+                # retrieve the first layer
+                l1 = disp.get_displayLayer(1)
+                l1.hide()
+                l1.clear()
+                l1.selectFont("Large.yfm")
+                if self.temperature.isOnline():
+                    msg = "%.1d %s" % (self.temperature.get_currentValue(), self.temperature.get_unit())
+                else:
+                    msg = "Unk"
+                # display a text in the middle of the screen
+                l1.drawText(w / 2, h / 4, YDisplayLayer.ALIGN.CENTER, msg)
+                if self.co2sensor.isOnline():
+                    msg = "%.1d %s" % (self.co2sensor.get_currentValue(), self.co2sensor.get_unit())
+                else:
+                    msg = "Unk"
+                # display a text in the middle of the screen
+                l1.drawText(w / 2, h / 4 * 3, YDisplayLayer.ALIGN.CENTER, msg)
+                l1.selectFont("Small.yfm")
+                l1.drawText(0, 0, YDisplayLayer.ALIGN.TOP_LEFT, "%d" % self._alive_counter)
+                self._alive_counter += 1
+                if self._alive_counter >= 10:
+                    self._alive_counter = 0
+                disp.swapLayerContent(3, 1)
+            except YAPI_Exception:
+                print("unable to display information on " + disp.get_friendlyName())
+
 
 def main():
     parser = argparse.ArgumentParser(description='Controller for a Velux KLF 200.')
@@ -142,21 +259,21 @@ def main():
                         help='Config files in JSON format')
     parser.add_argument("--reset_min_max", help="reset min an max value of C02 sensor",
                         action="store_true")
-    parser.add_argument('command', help="the command to execute\n Supported commandes are open/close/bind/co2")
+    parser.add_argument('command', help="the command to execute\n Supported commands are open/close/bind/co2/auto",
+                        default="auto")
     parser.add_argument('--zone', help="the name of the zone used. If not specified command are executed on all zones",
                         default=[], action='append')
     args = parser.parse_args()
     print(args)
-    controller = VeluxControler(args.config, args.zone, args.verbose)
+    controller = VeluxControler(args.config, args.verbose)
     if (args.reset_min_max):
         controller.reset_min_max()
-
     if args.command == 'open':
-        controller.open()
+        controller.open(args.zone)
     elif args.command == 'close':
-        controller.close()
+        controller.close(args.zone)
     elif args.command == 'auto':
-        controller.auto()
+        controller.auto2()
     elif args.command == 'co2':
         controller.read_co2()
     elif args.command == 'bind':
